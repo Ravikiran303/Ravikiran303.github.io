65250cc7da98b14cc2edd1d93e35a8b55e90db3f
RAVIKIRAN PILLI <ravi.pilli@moduluspi.com>
2020-07-14 14:27:44 +0530

    Products Demo

---
+++ precache-manifest.fdcacef35f447482e846276dfa2ecb35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c6f7a5c71c288677406fa88d662ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c7a25bd3952086bd56",
+    "url": "/static/css/2.0f36a402.chunk.css"
+  },
+  {
+    "revision": "6660f71c361f00942743",
+    "url": "/static/css/main.85bd88bc.chunk.css"
+  },
+  {
+    "revision": "98c7a25bd3952086bd56",
+    "url": "/static/js/2.3339b8d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3339b8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6660f71c361f00942743",
+    "url": "/static/js/main.cab59f6d.chunk.js"
+  },
+  {
+    "revision": "0ad793dc67828479f0e6",
+    "url": "/static/js/runtime-main.5939c274.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
